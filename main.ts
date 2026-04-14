3c0ebe33cf4b91a01b38dbc4565addac63853d16
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-04-14 19:38:36 -0400

    coded working verison 1.0

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Devon
+ * Created on: Sep 2026
+ * This program will use bluetooth. 
 */
 
-basic.showString('Hello, World!')
+// variables
+let objectDistance: number = 0
+let alreadyWarned: boolean = false
+
+// setup
+basic.showIcon(IconNames.Happy)
+radio.setGroup(247)
+
+// receiver code
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+
+// loop to read distance
+while (true) {
+    objectDistance = sonar.ping(
+        DigitalPin.P0,
+        DigitalPin.P1,
+        PingUnit.Centimeters
+    )
+    
+    // resets it
+    basic.clearScreen()
+    basic.showNumber(objectDistance)
+
+    // if it is less than 5 cm, displays way too close ONCE, doesn't spam it
+    if (objectDistance < 5 && alreadyWarned == false) {
+        radio.sendString("Way too close")
+        alreadyWarned = true
+    }
+
+    else {
+        alreadyWarned = false
+    }
+
+    basic.pause(1000)
+}
